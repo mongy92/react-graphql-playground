a0e25c860a4f10dc3ed7de72c641abed478cd92a
Ahmed Mongy <a.elzantot@gmail.com>
2019-03-24 12:05:19 +0200

    add server

--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from "./components/App";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient, InMemoryCache, HttpLink } from "apollo-boost";
+
+
+const httpLink = new HttpLink({
+    uri: 'http://localhost:4000'
+})
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+    <ApolloProvider client={client} >
+        <App />
+    </ApolloProvider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
